7dbff13c8729ba32d4e27a166d83181984e4646d
SaidMagomedow <saidmagom@mail.ru>
2022-03-26 01:05:19 +0300

    refactoring README

--- server/book_backend/app/core/config.py
+++ server/book_backend/app/core/config.py
@@ -1,7 +1,13 @@
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Union
 
-from typing import Any, Dict, List, Optional, Union
-
-from pydantic import AnyHttpUrl, BaseSettings, PostgresDsn, validator
+from pydantic import AnyHttpUrl
+from pydantic import BaseSettings
+from pydantic import PostgresDsn
+from pydantic import validator
 
 
 class Settings(BaseSettings):
@@ -33,7 +39,6 @@ class Settings(BaseSettings):
             host=values.get("POSTGRES_SERVER"),
             path=f"/{values.get('POSTGRES_DB') or ''}",
         )
-    
 
     class Config:
         case_sensitive = True
